f13ce010b00e5c37daf37e875bb58982bbf0e9d1
Kenneth Zhang <kzhang395@gmail.com>
2021-02-21 14:28:01 -0500

    Initial implementation of Header and Login page

--- persona-frontend/src/pages/LoginPage.jsx
+++ persona-frontend/src/pages/LoginPage.jsx
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Login page for application
 class LoginPage extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <h1>Login Page</h1>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Persona.io</h1>
+        <h5>Username: </h5>
+        <input type="text" />
+        <h5>Password: </h5>
+        <input type="password" />
+        <br />
+        <br />
+        <button class="btn-primary btn">Log in</button>
+        <h6>Log in with another account</h6>
+
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Log in with Google
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Log in with GitHub
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Log in with IU
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginPage;
