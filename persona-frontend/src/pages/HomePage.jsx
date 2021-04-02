a736ce2f192c8090e1de5adec6ffb4a2baad0ddd
sureshvsm <69179249+sureshvsm123@users.noreply.github.com>
2021-04-02 17:45:19 -0400

    Update HomePage.jsx

--- persona-frontend/src/pages/HomePage.jsx
+++ persona-frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ class HomePage extends Component {
     render() { 
         return ( 
             <div>
-                <h1>Home Page</h1>
+                <h1>Home Page test</h1>
             </div>
         );
     }
