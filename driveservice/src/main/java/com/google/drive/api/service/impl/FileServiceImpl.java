18155e7569be87cdf112d8fccd49ddea45cbe78e
Xiao Xiong <brockdw@outlook.com>
2021-04-16 04:39:19 -0400

    fixing file extension

--- driveservice/src/main/java/com/google/drive/api/service/impl/FileServiceImpl.java
+++ driveservice/src/main/java/com/google/drive/api/service/impl/FileServiceImpl.java
@@ -1,6 +1,5 @@
 package com.google.drive.api.service.impl;
 
-
 import com.google.drive.api.entity.Document;
 import com.google.drive.api.entity.User;
 import com.google.drive.api.exception.AppException;
@@ -28,108 +27,113 @@ import java.util.zip.ZipOutputStream;
 @Service
 public class FileServiceImpl implements FileService {
 
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
-    @Autowired
-    private GoogleDriveUtils googleDrive;
+	@Autowired
+	private GoogleDriveUtils googleDrive;
 
-    @Autowired
-    private DocumentRepo documentRepo;
+	@Autowired
+	private DocumentRepo documentRepo;
 
-    @Autowired
-    private UserRepo userRepo;
+	@Autowired
+	private UserRepo userRepo;
 
-    @Override
-    @Transactional
-    public void upload(MultipartFile[] files, String userName) throws Exception {
+	@Override
+	@Transactional
+	public void upload(MultipartFile[] files, String userName) throws Exception {
 
-        User user = userRepo.findByUserName(userName).orElseGet(() -> createNewUser(userName));
+		User user = userRepo.findByUserName(userName).orElseGet(() -> createNewUser(userName));
 
-        for (MultipartFile file : files) {
-            File targetFile = File.createTempFile(file.getOriginalFilename(), "");
-            try (OutputStream outStream = new FileOutputStream(targetFile)) {
-                outStream.write(file.getBytes());
-                String fileId = googleDrive.upload(targetFile, userName, file.getOriginalFilename(),
-                        file.getContentType());
-                Document document = new Document();
-                document.setFileId(fileId);
-                document.setFileName(file.getOriginalFilename());
-                document.setOwner(user);
-                Document dbDocument = documentRepo.save(document);
+		for (MultipartFile file : files) {
+			File targetFile = File.createTempFile(file.getOriginalFilename(), "");
+			try (OutputStream outStream = new FileOutputStream(targetFile)) {
+				outStream.write(file.getBytes());
+				String fileId = googleDrive.upload(targetFile, userName, file.getOriginalFilename(),
+						file.getContentType());
+				Document document = new Document();
+				document.setFileId(fileId);
+				document.setFileName(file.getOriginalFilename());
+				document.setOwner(user);
+				Document dbDocument = documentRepo.save(document);
 
-                user.addDocument(dbDocument);
+				user.addDocument(dbDocument);
 
-            } catch (Exception e) {
-                logger.error("Error while upload document to drive ", e);
-                throw new AppException("Error while upload document to drive ");
-            } finally {
-                targetFile.delete();
-            }
-            userRepo.save(user);
-        }
-    }
+			} catch (Exception e) {
+				logger.error("Error while upload document to drive ", e);
+				throw new AppException("Error while upload document to drive ");
+			} finally {
+				targetFile.delete();
+			}
+			userRepo.save(user);
+		}
+	}
 
-    @Override
-    public void deleteFileById(Long id) throws Exception {
-        Document dbDocument = documentRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("File not found id :" + id ));
+	@Override
+	public void deleteFileById(Long id) throws Exception {
+		Document dbDocument = documentRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("File not found id :" + id));
 
-        googleDrive.deleteFile(dbDocument.getFileId());
-        documentRepo.deleteById(id);
-    }
+		googleDrive.deleteFile(dbDocument.getFileId());
+		documentRepo.deleteById(id);
+	}
 
-    @Override
-    public void shareDocuments(ShareDocumentRequest shareDocumentRequest) {
+	@Override
+	public void shareDocuments(ShareDocumentRequest shareDocumentRequest) {
 
-        User userToShare = userRepo.findByUserName(shareDocumentRequest.getUsernameToShare())
-                .orElseThrow(() -> new ResourceNotFoundException("User not found username :" + shareDocumentRequest.getUsernameToShare()));
+		User userToShare = userRepo.findByUserName(shareDocumentRequest.getUsernameToShare())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"User not found username :" + shareDocumentRequest.getUsernameToShare()));
 
-        documentRepo.findAllById(shareDocumentRequest.getDocumentIds()).forEach(userToShare::addDocument);
-        userRepo.save(userToShare);
+		documentRepo.findAllById(shareDocumentRequest.getDocumentIds()).forEach(userToShare::addDocument);
+		userRepo.save(userToShare);
 
-    }
+	}
 
-    @Override
-    public ByteArrayOutputStream download(List<Long> ids, String userName) {
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            try (ZipOutputStream zos = new ZipOutputStream(baos)) {
-            	Set<String> distinctFileNames = new HashSet<>();
-                for (Long id : ids) {
-                    Document doc = documentRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("File not found"));
-                    String curFileName = doc.getFileName();
-                    String originalName = curFileName;
-                    for (int i = 1; !distinctFileNames.add(curFileName); i++) {
-                    	curFileName = originalName + "_"+ i;
-                    }
-                    ZipEntry entry = new ZipEntry(curFileName);
-                    zos.putNextEntry(entry);
-                    zos.write(googleDrive.download(doc.getFileId()).toByteArray());
-                }
-            }
-            return baos;
-        } catch (Exception e) {
-            logger.error("Error while download document ", e);
-            throw new AppException("Error while download document ");
-        }
-    }
+	@Override
+	public ByteArrayOutputStream download(List<Long> ids, String userName) {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			try (ZipOutputStream zos = new ZipOutputStream(baos)) {
+				Set<String> distinctFileNames = new HashSet<>();
+				for (Long id : ids) {
+					Document doc = documentRepo.findById(id)
+							.orElseThrow(() -> new ResourceNotFoundException("File not found"));
+					String curFileName = doc.getFileName();
+					String[] originalNameSplit = curFileName.split("\\.(?=[^\\.]+$)");
+					String originalName = originalNameSplit[0];
+					String extension = originalNameSplit[1];
+					for (int i = 1; !distinctFileNames.add(curFileName); i++) {
+						curFileName = originalName + "_" + i + "." + extension;
+					}
+					ZipEntry entry = new ZipEntry(curFileName);
+					zos.putNextEntry(entry);
+					zos.write(googleDrive.download(doc.getFileId()).toByteArray());
+				}
+			}
+			return baos;
+		} catch (Exception e) {
+			logger.error("Error while download document ", e);
+			throw new AppException("Error while download document ");
+		}
+	}
 
-    @Override
-    public Set<DocumentResponse> getFilesByUsername(String userName) {
-        User user = userRepo.findByUserName(userName)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found username :" + userName));
+	@Override
+	public Set<DocumentResponse> getFilesByUsername(String userName) {
+		User user = userRepo.findByUserName(userName)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found username :" + userName));
 
-        Set<DocumentResponse> documentResponses = user.getDocuments().stream()
-                .map(DocumentMapper::documentToDocumentResponse)
-                .collect(Collectors.toSet());
+		Set<DocumentResponse> documentResponses = user.getDocuments().stream()
+				.map(DocumentMapper::documentToDocumentResponse).collect(Collectors.toSet());
 
-        documentResponses.forEach(documentResponse -> documentResponse.setData(googleDrive.getByteArray(documentResponse.getFileId())));
-        return documentResponses;
+		documentResponses.forEach(
+				documentResponse -> documentResponse.setData(googleDrive.getByteArray(documentResponse.getFileId())));
+		return documentResponses;
 
-    }
+	}
 
-    private User createNewUser(String userName) {
-        User newUser = new User();
-        newUser.setUserName(userName);
-        return userRepo.save(newUser);
-    }
+	private User createNewUser(String userName) {
+		User newUser = new User();
+		newUser.setUserName(userName);
+		return userRepo.save(newUser);
+	}
 
 }
